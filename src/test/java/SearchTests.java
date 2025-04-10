1fc21981f6f85af06d0aeb1feffc2e1e24057c87
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-10 17:10:47 +0300

    3_tyulyaeva_done_1

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,24 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.*;
-
- class GoogleTests {
-    @Test
-    void selenideSearchTest ()
-    {
-        open("https://www.google.com/");
-        $(byName("q")).setValue("Selenide").pressEnter();
-        $("#search").shouldHave(text("selenide.org"));
-    }
-}
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.pageLoadStrategy ="eager";
+    }
+
+    @AfterEach
+    void closeWebDriver(){
+        Selenide.closeWebDriver();
+    }
+
     @Test
-    void successfulSearchTest() {
-        open("https://www.bing.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("html").shouldHave(text("https://selenide.org"));
+    void fillFormTest() {
+        open("automation-practice-form");
+        $("#firstName").setValue("Инна");
+        $("#lastName").setValue("Тюляева");
+        $("#userEmail").setValue("tyulyaeva.inna@yandex.ru");
+        $("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
+        $("#userNumber").setValue("79178332203");
+        $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").setValue("20 Jul 1991");
+        $("#subjectsInput").setValue("Comp").pressEnter();
+        $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
+        $("#uploadPicture").uploadFromClasspath("retouch.jpg");
+        $("#currentAddress").setValue("Санкт-Петербург").pressEnter();
+        $("#react-select-3-input").setValue("ncr").pressEnter();
+        $("#react-select-4-input").setValue("del").pressEnter();
+        $("#submit").click();
     }
 }
