f6b6becd5fa8dace4630f2dfff948efa4d842cb4
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-06-07 00:01:43 +0300

    myshop_form_1

--- src/test/java/test/jenkins_hw14/WebMyShopSteps.java
+++ src/test/java/test/jenkins_hw14/WebMyShopSteps.java
@@ -8,24 +8,63 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.error.ShouldHave.shouldHave;
 
 public class WebMyShopSteps {
-    @Step("Открыть страницу: https://my-shop.ru/")
+    @Step("Открыть страницу: https://www.labirint.ru/")
     public WebMyShopSteps openMyShopMainPage() {
         open("https://www.labirint.ru/");
         return this;
     }
 
-    @Step("Найти книгу 'Гиперион' автора 'Дэн Симмонс'")
+    @Step("Найти книгу 'Воскресение' автора 'Стивен Кинг'")
     public WebMyShopSteps myShopBookSearchTest() {
         $("[id=search-field").
-                setValue("Лев Толстой").pressEnter();
+                setValue("Стивен Кинг").pressEnter();
         $(".search-result")
-                .shouldHave(text("Воскресение"));
+                .shouldHave(text("11/22/63"));
         return this;
     }
 
-    @Step("Открыть меню 'Каталог'")
+    @Step("Перейти в подраздел 'Офисная канцелярия'")
     public WebMyShopSteps openCatalogMenu() {
-        $(".menu-title").shouldBe(visible).click();
+        $(".b-header-b-menu-e-list-item.b-toggle.b-header-b-menu-e-list-item-m-temp.analytics-click-js:nth-child(6) .b-header-b-menu-e-text")
+                .shouldBe(visible).click();
+        $(".col-xs-12.col-sm-6.col-md-4.col-xl-3.gutter-genre-list:nth-child(3) a")
+                .shouldBe(visible).click();
+        $(".genre-name")
+                .shouldHave(text("Офисная канцелярия"));
+        return this;
+    }
+
+    @Step("Перейти в раздел 'Книги'")
+    public WebMyShopSteps openSectionBooks() {
+        $(".b-header-b-menu-e-link.top-link-menu.first-child .b-header-b-menu-e-text").click();
+        return this;
+    }
+
+    @Step("Открыть карточку товара")
+    public WebMyShopSteps openProductCard() {
+        //$(".content-block.content-block-action").scrollIntoView(true).click();
+        $(".product-card.need-watch.watched.gtm-watched:first-child .product-card__name:nth-child(3)").click();
+        return this;
+    }
+
+    @Step("Добавить товар в корзину")
+    public WebMyShopSteps addPenBasketTest() {
+        $("._actions_zuu52_155").$("._button_ssd04_1").click();
+        $("._cartCount_1a3wc_104").shouldHave(text("1"));
+        return this;
+    }
+
+    @Step("Сменить текущую локацию на г.Волгоград'")
+    public WebMyShopSteps changeSity() {
+        $(".region-location-icon-txt").shouldBe(visible).click();
+        $(".g-alttext-deepblue:nth-child(3)").click();
+        return this;
+    }
+
+    @Step("Проверить, что установлен в качестве текущей локации г.Санкт-Петербург ")
+    public WebMyShopSteps checkChangeSityClick() {
+        $(".region-location-icon-txt")
+                .shouldHave(text("Санкт-Петербург "));
         return this;
     }
 }
--- src/test/java/test/jenkins_hw14/WebMyShopVacancyTests.java
+++ src/test/java/test/jenkins_hw14/WebMyShopVacancyTests.java
@@ -22,13 +22,67 @@ public class WebMyShopVacancyTests extends TestBase {
 
     @Tag("myshop_form")
     @Feature("VacancyJavaQa")
-    @Story("Тестирование формы сайта: https://my-shop.ru/")
+    @Story("Тестирование формы сайта: https://www.labirint.ru/")
     @Owner("inna_tyulyaeva")
     @Test
-    @DisplayName("Тест-кейс №1: Проверка поиска определенной серии книг")
+    @DisplayName("Тест-кейс №1: Проверка поиска книги")
     void searchResultsShouldContainDeterminedBookTest () {
         WebMyShopSteps steps = new WebMyShopSteps();
         steps.openMyShopMainPage()
              .myShopBookSearchTest();
     }
+
+    @Tag("bookvoed_form")
+    @Feature("VacancyJavaQa")
+    @Story("Тестирование формы сайта: https://www.labirint.ru/")
+    @Owner("inna_tyulyaeva")
+    @DisplayName("Тест-кейс №2: Проверка перехода в раздел 'Офисная канцелярия' с помощью меню 'Канцелярия'")
+    @Test
+    void checkMenuCatalogSectionTest() {
+        WebMyShopSteps steps = new WebMyShopSteps();
+        steps.openMyShopMainPage()
+                .openCatalogMenu();
+    }
+
+    @Tag("bookvoed_form")
+    @Feature("VacancyJavaQa")
+    @Story("Тестирование формы сайта: https://www.labirint.ru/")
+    @Owner("inna_tyulyaeva")
+    @DisplayName("Тест-кейс №3: Проверка просмотра первой карточки товара")
+    @Test
+    void checkProductCardTest() {
+        WebMyShopSteps steps = new WebMyShopSteps();
+        steps.openMyShopMainPage()
+                .myShopBookSearchTest()
+                .openProductCard();
+    }
+
+    @Tag("bookvoed_form")
+    @Feature("VacancyJavaQa")
+    @Story("Тестирование формы сайта: https://www.labirint.ru/")
+    @Owner("inna_tyulyaeva")
+    @DisplayName("Тест-кейс №4: Проверка добавления товара в корзину")
+    @Test
+    void checkPenBasketTest() {
+        WebMyShopSteps steps = new WebMyShopSteps();
+        steps.openMyShopMainPage()
+                .myShopBookSearchTest()
+                .openProductCard()
+                .addPenBasketTest();
+
+    }
+
+    @Tag("bookvoed_form")
+    @Feature("VacancyJavaQa")
+    @Story("Тестирование формы сайта: https://www.labirint.ru/")
+    @Owner("inna_tyulyaeva")
+    @DisplayName("Тест-кейс №5: Проверка смены города")
+    @Test
+    void changeLocationPersonTest() {
+        WebMyShopSteps steps = new WebMyShopSteps();
+        steps.openMyShopMainPage()
+                .changeSity()
+                .checkChangeSityClick();
+    }
+
 }
