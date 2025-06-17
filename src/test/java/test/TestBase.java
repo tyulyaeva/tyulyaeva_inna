dd7bfbda2ecb791fd89717e02da944da2dc6ab3b
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-06-17 17:05:13 +0300

    Labirint_fixed_PageObjectAdd_3

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import test.Attach.Attachments;
+import test.attach.Attachments;
 
 import java.util.Map;
 import java.util.UUID;
--- src/test/java/test/attach/Attachments.java
+++ src/test/java/test/attach/Attachments.java
@@ -1,4 +1,4 @@
-package test.Attach;
+package test.attach;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
